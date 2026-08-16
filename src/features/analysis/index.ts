9a64adec38d03c3a662afac168c5a062fa5f249c
ysonkhiya122 <yashsonkhiya2195@gmail.com>
2026-08-17 01:42:33 +0530

    codebase fix and lint error removed

--- src/features/analysis/index.ts
+++
@@ -1,1 +0,0 @@
-export { AnalysisDashboard } from '../../modules/analysis';
--- src/modules/drivers/DriversModule.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// Placeholder for Drivers module
-// Future implementation: Driver list, driver detail pages, statistics
-export const DriversModule: React.FC = () => {
-  return <div>Drivers Module - Coming Soon</div>;
-};
-
-export default DriversModule;
